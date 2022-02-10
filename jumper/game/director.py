e48aa82cfdc5c6c17721e43ccc20d15d0cfea5cc
AdrianRuizB18 <adrianruizb18@gmail.com>
2022-02-09 19:59:50 -0500

    player and dir class

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,5 +1,5 @@
 from jumper.game import word
-from jumper.game import draw  
+from jumper.game import jumper
 from jumper.game import player 
 from jumper.game import letter
 
@@ -11,19 +11,32 @@ class Main:
     while True:
         word_bank = word()
         player1 = player()
-        game = letter()
+        validation = letter()
         
         word_bank.pick_topic()
         word_bank.pick_word()
 
-        while word_bank.not_solved and player1.lives > 0:
-            while player1.guess_validation_incomplete:
+        lives = player1.get_lives()
+        
+
+        while word_bank.not_solved and lives > 0:
                 player1.guess()
-                game.validate_user_input(player1)
-                game.check_answer_update_lives(word_bank,player1)
-            print(word_bank.current_word_display)
-            player1.guess_validation_incomplete = True
-            word_bank.check_solve()
+                validation.validate_letter(player1.letter_guess)
+                while validation.correct == False:
+                    if validation.correct == True:
+                        validation.set_letter(player1.letter_guess)
+                        player1.letter_guess = validation.get_letter()
+                        break
+                    else:
+                        print("Error: Not a letter.")
+                        player1.guess()
+                        validation.validate_letter(player1.letter_guess)
+
+                    
+
+                print(word_bank.current_word_display)
+                player1.guess_validation_incomplete = True
+                word_bank.check_solve()
 
         if not word_bank.not_solved:
             print('\nYou win!')
--- jumper/game/letter.py
+++ jumper/game/letter.py
@@ -1,14 +1,20 @@
 class Letter:
     def __init__(self):
         self._letter = ""
-
+        self.correct = False
 
     def validate_letter(self, letter):
         """Validates if the user's input is a valid character, 
         then returns the letter in lowercase if it is. Returns false if it's not
         """
-        if letter.isalpha():
-            self._letter = letter.lower()
-            return self._letter
-        else: 
-            return False
+        if letter.isalpha() and letter.len() == 1:
+            self.correct = True
+        else:
+            self.correct = False
+
+    def set_letter(self, letter):
+        self._letter = letter.lower()
+    
+    def get_letter(self):
+        return self._letter
+
